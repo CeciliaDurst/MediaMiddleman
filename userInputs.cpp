3dd4cd1cc25340dfda7bcdfd68501fe8443add68
Joshua Nielsen <100550368+Joshua-Nielsen@users.noreply.github.com>
2023-08-01 17:39:44 -0400

    Add files via upload

---
+++ userInputs.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+
+	int releaseYear, runtime;
+
+	cout << "The MediaMiddleMan\n-----------------------\nSelect your preferences" <<endl;
+	cout << "Release Year: ";
+	cin >> releaseYear;
+	cout << "Run Time: ";
+	cin >> runtime;
+
+	return 0;
+
+}
