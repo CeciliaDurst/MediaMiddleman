d125fb5e1e235ab7ce08d2cce3e5dba79b43d7c5
Joshua Nielsen <100550368+Joshua-Nielsen@users.noreply.github.com>
2023-08-02 15:54:47 -0400

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 #include <string>
-#include <list>
+#include <vector>
 #include <unordered_map>
 using namespace std;
 
-// first input is unordered map containing <key, list of information on the movie at this key (for testing im using 4 search requests Ex. format, title, release year, runtime
-// second input is a list of user inputs
+// first input is unordered map containing <key, vector of information on the movie at this key (for testing im using 4 search requests Ex. format, title, release year, runtime
+// second input is a vector of user inputs
 // returns a list of keys that match the search requests
-list<string> mapFilter(unordered_map<string, list<string>> allContent, list<string> userInputs) {
+vector<string> mapFilter(unordered_map<string, vector<string>> allContent, vector<string> userInputs) {
 
-	list<string> currList;	
-	list<string> correctKeys;
+	vector<string> currList;
+	vector<string> correctKeys;
 	int count;
 
 	for (auto& i : allContent) { // i loops through every key of allContent
 
 		count = 0;
 		currList = i.second;
-		for (auto const& j : currList) { // j loops through the list of the current key
+		for (auto const& j : currList) { // j loops through the vector of the current key
 
 			count++;
 			for (auto const& n : userInputs) { // n loops through every user search request
@@ -29,7 +29,7 @@ list<string> mapFilter(unordered_map<string, list<string>> allContent, list<stri
 				}
 				else if (count == 4) { // making sure we checked every preference. For testing I used 4 preferences
 
-					correctKeys.push_back(i.first); // adding key to list of correct keys
+					correctKeys.push_back(i.first); // adding key to vector of correct keys
 					break;
 
 				}
@@ -44,17 +44,17 @@ list<string> mapFilter(unordered_map<string, list<string>> allContent, list<stri
 
 }
 
-int main() {
+int main() { // main for testing inputs and outputs
 
-	list<string> userInputs = {"correct", "correct", "correct", "correct"};
-	unordered_map<string, list<string>> allContent;
-	list<string> tempList = {"correct", "correct", "correct", "correct"};
+	vector<string> userInputs = {"correct", "correct", "correct", "correct"};
+	unordered_map<string, vector<string>> allContent;
+	vector<string> tempList = {"correct", "correct", "correct", "correct"};
 	allContent["abcd"] = tempList;
 	tempList = {"wrong", "wrong", "wrong", "wrong"};
 	allContent["efgh"] = tempList;
 	allContent["hijk"] = tempList;	
 
-	list<string> correctKeys = mapFilter(allContent, userInputs);
+	vector<string> correctKeys = mapFilter(allContent, userInputs);
 
 	for (auto const& i : correctKeys) {
 
