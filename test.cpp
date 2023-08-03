7f2548ac5aeae699c0dc60e4c13645bd19345ef2
masterofbanks <tarikalex2003@gmail.com>
2023-08-03 07:27:21 -0400

    Added Content CLass

--- test.cpp
+++ test.cpp
@@ -1,8 +1,64 @@
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
+class Content{
+    private:
+        string format;
+        string title;
+        int startYear;
+        int endYear;
+        int runTimeMins;
+        vector<string> genres;
+    public:
+        Content();
+        Content(string f, string t, int sY, int eY, int rTM, vector<string> genres);
+        void setGenres(string genreStream);
+        string getFormat();
+        string getTitle();
+        int getStartYear();
+        int getEndYear();
+        int getRunTimeMins();
+        string RunTimeMins_to_Hours_and_Mins();
+        void outputDetails();
+        
+};
+
+Content::Content(){
+    format = "";
+    title = "";
+    startYear = 0;
+    endYear = 0;
+    runTimeMins = 0;
+    genres = {};
+}
+
+Content::Content(string f, string t, int sY, int eY, int rTM, vector<string> genres){
+    format = f;
+    title = t;
+    startYear = sY;
+    endYear = eY;
+    runTimeMins = rTM;
+    genres = {};
+}
+
+void Content::setGenres(string genreStream){
+    int index = 0;
+    while(genreStream.size() != 0){
+        index = genreStream.find_first_of(',');
+
+    }
+}
+
+
+
+
+
 int main (){
-    cout << "Hello world";
+    string test = "Horror, Adventure, Comedy";
+    int index = test.find_first_not_of(',');
+    cout << test.substr(0,index) << endl;    
     return 0;
 }
 
