57f4dc24627f947dacf3357149363089e2556e7c
Joshua Nielsen <100550368+Joshua-Nielsen@users.noreply.github.com>
2023-08-03 16:09:31 -0400

    Update userInputs.cpp

--- userInputs.cpp
+++ userInputs.cpp
@@ -3,24 +3,95 @@
 #include <vector>
 using namespace std;
 
+class Content {
+private:
+    string format;
+    string title;
+    int startYear;
+    int endYear;
+    int runTimeMins;
+    vector<string> genres;
+public:
+    Content();
+    Content(string f, string t, int sY, int eY, int rTM);
+
+};
+
+Content::Content() {
+    format = "";
+    title = "";
+    startYear = 0;
+    endYear = 0;
+    runTimeMins = 0;
+    genres = {};
+}
+
+Content::Content(string f, string t, int sY, int eY, int rTM) {
+    format = f;
+    title = t;
+    startYear = sY;
+    endYear = eY;
+    runTimeMins = rTM;
+    genres = {};
+}
+
 int main () {
 
-	string title, runtime, releaseYear, genre;
+    string format, title, strStartYear, strEndYear, strRuntime;
+    int startYear = 0, endYear = 0, runtime = 0;
 
 	cout << "The MediaMiddleMan\n-------------------------------------\nSelect your preferences or type None" <<endl;
+
+    cout << "Format: ";
+    cin >> format;
+
 	cout << "Title: ";
 	cin >> title;
 
-	cout << "Run Time: ";
-	cin >> runtime;
+    cout << "Release Year: ";
+    cin >> strStartYear;
 
-	cout << "Release Year: ";
-	cin >> releaseYear;
-	
-	cout << "Genre: ";
-	cin >> genre;
+    if (strStartYear == "None") {
 
-	vector<string> userInputs = {title, runtime, releaseYear, genre};
+        startYear == -1;
+
+    }
+    else {
+
+        startYear = stoi(strStartYear);
+
+    }
+
+    cout << "End Year of Series: ";
+    cin >> strEndYear;
+
+    if (strEndYear == "None") {
+
+        endYear == -1;
+
+    }
+    else {
+
+        endYear = stoi(strEndYear);
+
+    }
+
+    cout << "Run Time in Minutes: ";
+    cin >> strRuntime;
+
+    if (strRuntime == "None") {
+
+        runtime == -1;
+
+    }
+    else {
+
+        runtime = stoi(strRuntime);
+
+    }
+
+    Content userInputs;
+	userInputs = Content(format, title, startYear, endYear, runtime);
 
 	return 0;
 
